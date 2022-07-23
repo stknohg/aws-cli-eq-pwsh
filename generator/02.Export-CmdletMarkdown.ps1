3ff432b38bd0ad660db501cc70eb0a96669daf36
stknohg <stknohg@gmail.com>
2022-07-23 15:28:03 +0900

    Change CHANGELOG url to raw  url.

--- generator/02.Export-CmdletMarkdown.ps1
+++ generator/02.Export-CmdletMarkdown.ps1
@@ -43,8 +43,8 @@ function Export-TopPageMarkdown {
         ""
         "### Version"
         ""
-        "* [AWS CLI](https://aws.amazon.com/cli/): {0} ([CHANGELOG](https://github.com/aws/aws-cli/blob/v2/CHANGELOG.rst))" -f (Get-Content -LiteralPath $CLIVersionMetadataPath -Encoding ascii)
-        "* [AWS Tools for PowerShell](https://aws.amazon.com/powershell/): {0} ([CHANGELOG](https://github.com/aws/aws-tools-for-powershell/blob/master/CHANGELOG.md))" -f $cmdletVersion
+        "* [AWS CLI](https://aws.amazon.com/cli/): {0} ([CHANGELOG](https://raw.githubusercontent.com/aws/aws-cli/v2/CHANGELOG.rst))" -f (Get-Content -LiteralPath $CLIVersionMetadataPath -Encoding ascii)
+        "* [AWS Tools for PowerShell](https://aws.amazon.com/powershell/): {0} ([CHANGELOG](https://raw.githubusercontent.com/aws/aws-tools-for-powershell/master/CHANGELOG.md))" -f $cmdletVersion
         ""
         "### Contents"
         ""

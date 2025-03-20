ee9abb0514d9138c01377aa62cabe2a4e5b8cd2a
stknohg <stknohg@gmail.com>
2025-03-20 19:45:49 +0900

    Delete docs directory.

--- docs/js/base.js
+++
@@ -1,7 +0,0 @@
-function setColorScheme() {
-    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (isDarkMode) {
-        document.querySelector("html").setAttribute("class", "dark");
-    }
-}
-setColorScheme();
